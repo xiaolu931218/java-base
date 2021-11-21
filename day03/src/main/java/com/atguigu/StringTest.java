9630ccb7a39a9d24a2355b9aa1090330860cbdd9
xiaolu <1324790234@qq.com>
2021-11-21 22:34:18 +0800

    测试String的类

--- day03/src/main/java/com/atguigu/StringTest.java
+++ day03/src/main/java/com/atguigu/StringTest.java
@@ -9,15 +9,23 @@ public class StringTest {
 
     @Test
     public void test01() {
-        final int a=1;
-        final int b =2;
-        int c = a+b;
-        System.out.println(a);
-        System.out.println(c);
 
-        final String s1 = "a";
-        final String s2 = "a";
-        String s3 = s1 + s2;
+        /**
+         * 1、常量与常量的拼接结果在常量池，且常量池中不会存在相同内容的结果
+         * 2、只要其中一个是变量，结果就在堆中
+         * 3、如果拼接的结果调用intern()方法，返回值就在常量池
+         */
+        String s1 = "hello";
+        String s2 = "world";
+        String s3 = "hello" + "world";
+        String s4 = s1 + "world";
+        String s5 = s1 + s2;
+        String s6 = (s1 + s2).intern();
+
+        System.out.println(s3 == s4);//false
+        System.out.println(s3 == s5);//false
+        System.out.println(s4 == s5);//false
+        System.out.println(s3 == s6);//true
 
     }
 }
